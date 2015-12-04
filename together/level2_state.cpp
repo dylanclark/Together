74e2e9bd6b140102b779a8b25cbadd8f6ded67c2
dylanclark <dylanmcdanielclark@gmail.com>
2015-12-04 15:31:18 -0500

    levels 1 and 2 now complete!!1

--- together/level2_state.cpp
+++ together/level2_state.cpp
@@ -104,16 +104,7 @@ void level2_state::draw(engine* game)
     }
     
     b_char.render(&camera->display, game);
-    w_char.render(&camera->display, game);
     b_level_end.render(&camera->display, game);
-    w_level_end.render(&camera->display, game);
-    b_button.render(&camera->display, game);
-    w_button.render(&camera->display, game);
-    b_springboard.render(&camera->display, game);
-    w_springboard.render(&camera->display, game);
-    b_cross_spring.render(&camera->display, game);
-    // level2_end.render(&camera->display, game->rend);
-
 
     SDL_RenderPresent(game->rend);
 }
@@ -198,48 +189,13 @@ void level2_state::load_textures(engine* game)
         printf("Failed to load  white level end texture!\n");
         return;
     }
-    if(!b_button_tex.load_tile_sheet("textures/black/button/b_button.png", game->rend))
-    {
-        printf("Failed to load black button texture!\n");
-        return;
-    }
-    if(!w_button_tex.load_tile_sheet("textures/white/button/w_button.png", game->rend))
-    {
-        printf("Failed to load white button texture!\n");
-        return;
-    }
-    if(!b_springboard_tex.load_tile_sheet("textures/black/spring/b_spring.png", game->rend))
-    {
-        printf("Failed to load black springboard texture!\n");
-        return;
-    }
-    if(!w_springboard_tex.load_tile_sheet("textures/white/spring/w_spring.png", game->rend))
-    {
-        printf("Failed to load white springboard texture!\n");
-        return;
-    }
-    if(!w_cross_spring_tex.load_tile_sheet("textures/white/cross_layer/w_cross.png", game->rend))
-    {
-        printf("Failed to load white cross layer spring texture!\n");
-        return;
-    }
-    if(!b_cross_spring_tex.load_tile_sheet("textures/black/cross_layer/b_cross.png", game->rend))
-    {
-        printf("Failed to black white cross layer spring texture!\n");
-        return;
-    }
-    //if (!level2_end_tex.load_message(128, 16, "textures/levels/level_2_done.png", game->rend))
-    {
-     //   printf("Failed to load level 2 end texture!\n");
-     //   return;
-    }
 
     
     // initialize level
     width = 27;
-    height = 19;
+    height = 18;
     
-    path = "levels/level_002.csv";
+    path = "levels/level_02.csv";
     
     if (!set_tiles(tileset, path, width, height))
     {
@@ -257,57 +213,14 @@ void level2_state::init_objects(engine* game)
     b_char.col_rect.y = 8 * TILE_WIDTH;
     b_char.black = true;
     
-    // initialize white dot
-    w_char.status = CHAR_INACTIVE;
-    w_char.tex = w_char_tex;
-    w_char.col_rect.x = 2 * TILE_WIDTH;
-    w_char.col_rect.y = 9 * TILE_WIDTH;
-    w_char.black = false;
-    
-    // init crate #1
-    crates.push_back(new crate(5 * TILE_WIDTH, 7  * TILE_WIDTH, FOUR_BY_TWO));
-    crates.back()->tex = crate_tex_four_by_two;
-    crates.back()->black = true;
-    
     camera = new class camera(game->screen_width, game->screen_height);
     
-    // initialize black button
-    b_button.tex = b_button_tex;
-    b_button.col_rect.x = 1200;
-    b_button.col_rect.y = 480;
-    b_button.single = true;
-    b_button.direction = RIGHT;
+    // initialize black level end
+    b_level_end.tex = b_end_tex;
+    b_level_end.col_rect.x = 1500;
+    b_level_end.col_rect.y = 11 * TILE_WIDTH;
     
-    // initialize white button
-    w_button.tex = w_button_tex;
-    w_button.col_rect.x = 200;
-    w_button.col_rect.y = 540;
-    w_button.direction = UP;
-    w_button.status = BUTT_INACTIVE;
-    
-    // initialize black springboard
-    b_springboard.tex = b_springboard_tex;
-    b_springboard.col_rect.x = 920;
-    b_springboard.col_rect.y = 480;
-    b_springboard.show = true;
-    b_springboard.direction = FLIP_RIGHT;
-    b_springboard.x_spring = 0;
-    b_springboard.y_spring = 10;
-    
-    // initialize white springboard
-    w_springboard.tex = w_springboard_tex;
-    w_springboard.col_rect.x = 920;
-    w_springboard.col_rect.y = 540;
-    w_springboard.show = true;
-    w_springboard.direction = FLIP_RIGHT;
-    w_springboard.x_spring = 0;
-    w_springboard.y_spring = 10;
-    
-    //level2_end.tex = level2_end_tex;
-    //level2_end.send_message(END, game);
-    
-    camera = new class camera(game->screen_width, game->screen_height);
-    
+
 
 }
 
@@ -315,7 +228,7 @@ void level2_state::interactions(engine* game)
 {
     
     // if both are on level end object
-    if(b_level_end.check(b_char.col_rect) && w_level_end.check(w_char.col_rect))
+    if(b_level_end.check(b_char.col_rect))
     {
         b_char.center(&b_level_end.col_rect);
         w_char.center(&w_level_end.col_rect);
@@ -324,118 +237,4 @@ void level2_state::interactions(engine* game)
         change_state(game, new level3_state);
     }
     
-    //if black button is activated
-    if(b_button.check(b_char.col_rect))
-    {
-        // used
-        b_button.used = true;
-        
-        // activate
-        b_button.activated = true;
-        
-        // animate
-        if(b_button.status == BUTT_INACTIVE)
-        {
-            b_button.status = (b_button.status + 1) % 4;
-        }
-        
-        // initialize black level end
-        b_level_end.tex = b_end_tex;
-        b_level_end.col_rect.x = 1500;
-        b_level_end.col_rect.y = 480;
-        
-        // initialize black level end
-        w_level_end.tex = w_end_tex;
-        w_level_end.col_rect.x = 1500;
-        w_level_end.col_rect.y = 540;
-
-        
-    }
-    else
-    {
-        b_button.activated = false;
-        
-        if(b_button.status != BUTT_INACTIVE)
-        {
-            b_button.status = (b_button.status + 1) % 4;
-        }
-    }
-    
-    //if white button is activated
-    if(w_button.check(w_char.col_rect))
-    {
-        // used
-        w_button.used = true;
-        
-        // activate
-        w_button.activated = true;
-        
-        if(w_button.status == BUTT_INACTIVE)
-        {
-            w_button.status = (w_button.status + 1) % 4;
-        }
-    }
-    else
-    {
-        w_button.activated = false;
-        
-        if(w_button.status == BUTT_ACTIVE)
-        {
-            w_button.status = (w_button.status + 1) % 4;
-        }
-        //if(w_button.status != BUTT_INACTIVE)
-        {
-           //w_button.status = (w_button.status + 1) % 4;
-        }
-    }
-    
-    //if black springboard is activated
-    if(b_springboard.check(b_char.col_rect) && b_springboard.show)
-    {
-        if(b_char.col_rect.y < b_springboard.col_rect.y + 20)
-        {
-            // activate
-        b_springboard.activated = true;
-        
-        if(b_springboard.status == BOARD_INACTIVE)
-        {
-            b_springboard.status = (b_springboard.status + 1) % 4;
-        }
-        
-        b_char.spring(b_springboard.x_spring, b_springboard.y_spring, b_springboard.direction);
-        }
-    }
-    else
-    {
-        b_springboard.activated = false;
-        
-        if(b_springboard.status != BOARD_INACTIVE)
-        {
-            b_springboard.status = (b_springboard.status + 1) % 4;
-        }
-    }
-    
-    //if white springboard is activated
-    if(w_springboard.check(w_char.col_rect) && w_springboard.show)
-    {
-        // activate
-        w_springboard.activated = true;
-
-        if(w_springboard.status == BOARD_INACTIVE)
-        {
-            w_springboard.status = (w_springboard.status + 1) % 4;
-        }
-        
-        w_char.spring(w_springboard.x_spring, w_springboard.y_spring, w_springboard.direction);
-        
-    }
-    else
-    {
-        w_springboard.activated = false;
-        
-        if(w_springboard.status != BOARD_INACTIVE)
-        {
-            w_springboard.status = (w_springboard.status + 1) % 4;
-        }
-    }
 }
