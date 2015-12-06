1fc5e05bbb70c70169deb4fbf8680e033f77fedd
fraserweist <fraserweist@gmail.com>
2015-12-06 02:28:15 -0500

    Added levels

--- together/characters.cpp
+++ together/characters.cpp
@@ -534,7 +534,8 @@ bool dot::crate_col(levelstate* level)
             {
                 if (!level->crates[i]->black)
                 {
-                    level->shiftable = tile_col(level->crates[i]->tileset, MAX_BORDER);
+                    level->shiftable = false;
+                    tile_col(level->crates[i]->tileset, MAX_BORDER);
                     
                     return true;
                 }
@@ -592,6 +593,7 @@ bool dot::crate_col(levelstate* level)
             {
                 if (level->crates[i]->black)
                 {
+                    level->shiftable = false;
                     tile_col(level->crates[i]->tileset, MAX_BORDER);
                     
                     return true;
